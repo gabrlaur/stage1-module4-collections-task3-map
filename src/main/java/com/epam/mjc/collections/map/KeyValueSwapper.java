98fb15b56f19e8d4fd6a03fc349101e69ecefce7
gabrlaur <45432522+gabrlaur@users.noreply.github.com>
2022-11-02 20:23:24 +0200

    implement methods

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,16 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> newMap = new HashMap<>();
+        for (var entry : sourceMap.entrySet()) {
+            if (!newMap.containsKey(entry.getValue())) {
+                newMap.put(entry.getValue(), entry.getKey());
+            }
+        }
+        return newMap;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,21 @@
 package com.epam.mjc.collections.map;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        String[] words = sentence.toLowerCase().replace('.', ' ').replace(',', ' ').split(" ");
+        Map<String, Integer> repetitionMap = new LinkedHashMap<>();
+        for (String word : words) {
+            if (!word.trim().equals("")) {
+                if (repetitionMap.containsKey(word)) {
+                    repetitionMap.put(word, repetitionMap.get(word) + 1);
+                } else {
+                    repetitionMap.put(word, 1);
+                }
+            }
+        }
+        return repetitionMap;
     }
 }
